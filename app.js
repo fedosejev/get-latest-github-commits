f873ca2ad909e092cadb37f14193a87afc3a2d64
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-03-13 13:28:15 +0000

    Update

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ var GITHUB_AUTH = fileJson.gitHub;
 
 var gitHub = new GitHubApi({
   // required
-  version: "3.0.0",
+  version: '3.0.0',
   // optional
   debug: false,
-  protocol: "https",
-  host: "api.github.com", // should be api.github.com for GitHub
-  pathPrefix: "", // for some GHEs; none for GitHub
+  protocol: 'https',
+  host: 'api.github.com', // should be api.github.com for GitHub
+  pathPrefix: '', // for some GHEs; none for GitHub
   timeout: 5000,
   headers: {
-    "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
+    'user-agent': 'My-Cool-GitHub-App' // GitHub is happy with a unique user agent
   }
 });
 
